e3f8ec62a03ad1df77ef59454b1e0f9d5d324b01
Maxime Burlat <maximeburlat@gmail.com>
2019-03-22 15:47:21 -0500

    implemented and tested the simple neural network

--- index.py
+++ index.py
@@ -2,6 +2,8 @@
 import pandas as pd
 import numpy as np
 
+from NeuralNetwork import NeuralNetwork
+
 # Package imports
 # Matplotlib
 import matplotlib
@@ -13,3 +15,17 @@ import sklearn.datasets
 import sklearn.linear_model
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.metrics import accuracy_score
+
+
+X = np.array([[0, 0, 1],
+              [0, 1, 1],
+              [1, 0, 1],
+              [1, 1, 1]])
+y = np.array([[0], [1], [1], [0]])
+nn = NeuralNetwork(X, y)
+
+for i in range(10000):
+    nn.feedforward()
+    nn.backprop()
+
+print(nn.output)
