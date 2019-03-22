46b6087c4fee4a283d10c3cf3230459ee5ca1417
Maxime Burlat <maximeburlat@gmail.com>
2019-03-22 15:49:09 -0500

    removed useless package for this sample

--- index.py
+++ index.py
@@ -4,18 +4,6 @@ import numpy as np
 
 from NeuralNetwork import NeuralNetwork
 
-# Package imports
-# Matplotlib
-import matplotlib
-import matplotlib.pyplot as plt
-# SciKitLearn is a machine learning utilities library
-import sklearn
-# The sklearn dataset module helps generating datasets
-import sklearn.datasets
-import sklearn.linear_model
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.metrics import accuracy_score
-
 
 X = np.array([[0, 0, 1],
               [0, 1, 1],
