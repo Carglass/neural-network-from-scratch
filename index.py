c80a3d1f2717361abeaacc2ebfdfbeb0305c492b
Maxime Burlat <maximeburlat@gmail.com>
2019-03-22 11:38:04 -0500

    initial files

---
+++ index.py
@@ -0,0 +1,15 @@
+# importing all the libraries and dataset
+import pandas as pd
+import numpy as np
+
+# Package imports
+# Matplotlib
+import matplotlib
+import matplotlib.pyplot as plt
+# SciKitLearn is a machine learning utilities library
+import sklearn
+# The sklearn dataset module helps generating datasets
+import sklearn.datasets
+import sklearn.linear_model
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import accuracy_score
